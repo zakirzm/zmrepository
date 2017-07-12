1e226849fc1d8ed821f36e83be6cd06f7c310256
gouse <gouse.krnl@gmail.com>
2017-07-12 23:40:39 +0530

    Added new method

--- TestNgPrj/src/test/java/com/TestngPkg/TestNgPrj/DemoCls.java
+++ TestNgPrj/src/test/java/com/TestngPkg/TestNgPrj/DemoCls.java
@@ -2,6 +2,10 @@ package com.TestngPkg.TestNgPrj;
 
 public class DemoCls {
 	
+	public void mc(){
+		System.out.println("instance method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Class1");
 
