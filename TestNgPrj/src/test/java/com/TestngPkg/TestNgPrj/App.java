4f2705e5f8492224b7676891475858f8f83f5031
gouse <gouse.krnl@gmail.com>
2017-07-12 12:53:18 +0530

    frst program

---
+++ TestNgPrj/src/test/java/com/TestngPkg/TestNgPrj/App.java
@@ -0,0 +1,16 @@
+package com.TestngPkg.TestNgPrj;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public  void ecMth()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
